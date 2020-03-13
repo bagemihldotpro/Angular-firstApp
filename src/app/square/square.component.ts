a5dc50880d9b9b84685323e692643b163b2d7c2f
crisbagemihl <cristian@accolade.fi>
2020-03-13 23:05:47 +0000

    Theme - fix the with/height ratio - css CB

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -7,7 +7,10 @@ import { Component, Input } from '@angular/core';
     <button *ngIf="value=='X'" hero nbButton status="success">{{ value != null ? value : "&nbsp;" }}</button>
     <button hero nbButton *ngIf="value=='O'" status="warning">{{ value != null ? value : "&nbsp;" }}</button>
   `,
-  styles: ['button { line-height: 100%; width: 100%; height: 100%; font-size: 5em !important; }']
+  styles: [
+    'app-square { width: 100%; height: 100%; }',
+    'button {  width: 100%; height: 100%; font-size: 5em !important; }'
+  ]
 })
 export class SquareComponent  {
   @Input() value;
