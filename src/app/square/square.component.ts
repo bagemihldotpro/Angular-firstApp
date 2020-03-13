6a269d1cea1f3a76654566710d064719330b3901
crisbagemihl <cristian@accolade.fi>
2020-03-13 15:26:31 +0000

    Theme button squares base color to red "danger" CB

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,9 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button hero nbButton *ngIf="value==null">{{ value != null ? value : "&nbsp;" }}</button>
-    <button *ngIf="value=='X'" hero nbButton>{{ value != null ? value : "&nbsp;" }}</button>
-    <button hero nbButton *ngIf="value=='O'">{{ value != null ? value : "&nbsp;" }}</button>
+    <button hero nbButton *ngIf="value==null" status="danger">{{ value != null ? value : "&nbsp;" }}</button>
+    <button *ngIf="value=='X'" hero nbButton status="success">{{ value != null ? value : "&nbsp;" }}</button>
+    <button hero nbButton *ngIf="value=='O'" status="warning">{{ value != null ? value : "&nbsp;" }}</button>
   `,
   styles: ['button { line-height: 100%; width: 100%; height: 100%; font-size: 5em !important; }']
 })
